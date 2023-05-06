fa159a07ad7b6761f6e87da8edbf6f2022304bf6
mekshuniana <mekshuniana@mail.ru>
2023-05-07 01:44:27 +0300

    Fix-proj

---
+++ src/components/Form.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { useForm } from "react-hook-form";
+
+const Form = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example"));
+
+  return (
+    <div>
+     <form id='form' onSubmit={handleSubmit(onSubmit)}>
+       <h1>Заполните форму для обратной связи</h1>
+       <div className='input-group mb-3'>
+          <input
+          {...register('lastName',{
+            maxLength:50,
+            pattern: /^[А-Яа-я]+$/i
+          })}
+          type="text"
+          className='form-control'
+          placeholder='Фамилия'/>
+       </div>
+       {errors?.lastName?.type === 'required'&&(
+        <p>Поле обязательно для заполнения</p>
+       )}
+
+       {errors?.lastName?.type === 'maxLength'&&(
+        <p>Поле не может содержать более 50 символов</p>
+       )}
+
+       {errors?.lastName?.type === 'pattern'&&(
+        <p>Поле заполнено некорректно</p>
+       )}
+
+     <div className='input-group mb-3'>
+        <input
+          {...register('firstName',{
+            maxLength:50,
+            pattern: /^[А-Яа-я]+$/i
+          })}
+          type="text"
+          className='form-control'
+          placeholder='Имя'/>
+       </div>
+       {errors?.firstName?.type === 'required'&&(
+        <p>Поле обязательно для заполнения</p>
+       )}
+
+       {errors?.firstName?.type === 'maxLength'&&(
+        <p>Поле не может содержать более 50 символов</p>
+       )}
+
+       {errors?.firstName?.type === 'pattern'&&(
+        <p>Поле заполнено некорректно</p>
+       )}
+
+    <div className='input-group mb-3'>
+         <input
+          {...register('patronymic',{
+            maxLength:50,
+            pattern: /^[А-Яа-я]+$/i
+          })}
+          type="text"
+          className='form-control'
+          placeholder='Отчество'/>
+       </div>
+       {errors?.patronymic?.type === 'required'&&(
+        <p>Поле обязательно для заполнения</p>
+       )}
+
+       {errors?.patronymic?.type === 'maxLength'&&(
+        <p>Поле не может содержать более 50 символов</p>
+       )}
+
+       {errors?.patronymic?.type === 'pattern'&&(
+        <p>Поле заполнено некорректно</p>
+       )}
+
+    <div className='input-group mb-3'>
+         <input
+          {...register('email',{
+            maxLength:50,
+            pattern: /^[A-Za-z@._-]+$/i
+          })}
+          type="text"
+          className='form-control'
+          placeholder='Email'/>
+       </div>
+       {errors?.email?.type === 'required'&&(
+        <p>Поле обязательно для заполнения</p>
+       )}
+
+       {errors?.email?.type === 'maxLength'&&(
+        <p>Поле не может содержать более 50 символов</p>
+       )}
+
+       {errors?.email?.type === 'pattern'&&(
+        <p>Поле заполнено некорректно</p>
+       )}
+       <div>
+        <input className='btn btn-outline-primary' type="submit" />
+       </div> 
+     </form>
+    </div>
+  )
+}
+
+export default Form
